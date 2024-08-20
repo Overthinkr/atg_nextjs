d73eb6afae733126637456d244c864ec79ad444b
Mayank Roy <roy050703@gmail.com>
2024-08-20 20:00:20 +0530

    final responsive fixes

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -73,10 +73,13 @@ export default function Landing() {
                         <span className="material-symbols-outlined text-blue">format_quote</span> Testimonials
                     </h4>
                     <p className="card-text">In a fast growing and ever changing city like Bangalore, it sometimes becomes very difficult to find or connect with like minded people. Websites like hobbycue.com is a great service which helps me get in touch with, communicate, connect, and exchange ideas with other dancers. It also provides the extra benefit of finding products and services that I can avail, which I can be assured is going to be of great quality as it comes recommended by people of the hobbycue community. To have discussions, to get visibility, and to be able to safely explore various hobbies and activities in my city, all under one roof, is an excellent idea and I highly recommend it.</p>
-                    <audio controls>
-                        <source src="horse.ogg" type="audio/ogg" />
-                        Your browser does not support the audio tag.
-                    </audio>
+                    <div className="responsive-audio">
+                        <audio controls>
+                            <source src="horse.ogg" type="audio/ogg" />
+                            Your browser does not support the audio tag.
+                        </audio>
+                    </div>
+
                 </div>
             </div>
         </div>
