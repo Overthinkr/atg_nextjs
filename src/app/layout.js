f7cef1a00377aeceb30614eb12e70d3edb9e1df7
Mayank Roy <roy050703@gmail.com>
2024-08-20 20:12:14 +0530

    es lint link tag bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,10 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=optional"
-        />
+        /> */}
       </head>
       <body className={inter.className}>
         <Navbar />
